e5da7b8e11be6c73b9d0a269b632871e09013ac0
Adam Owada <adamowada@gmail.com>
2024-08-31 01:08:27 -0700

    removed rss feed.xml, renaming articles -

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -11,11 +11,6 @@ export const metadata = {
   },
   description:
     "Hi, I'm Adam, a fullstack software engineer and AI tinkerer based in Seattle. I'm the co-founder and sole developer of Observe Safety, an enterprise SaaS platform designed to help general contractors efficiently track and manage safety data.",
-  alternates: {
-    types: {
-      'application/rss+xml': `${process.env.NEXT_PUBLIC_SITE_URL}/feed.xml`,
-    },
-  },
 }
 
 export default function RootLayout({ children }) {
